05f245834e4d9a561161e74256352986ca01f96d
Muhammed Yusuf <velid@DESKTOP>
2020-12-02 12:28:33 +0300

    Tablo çıkartma ve düzenleme bitti.

---
+++ chart.py
@@ -0,0 +1,11 @@
+from pandas.core.frame import DataFrame
+import tabula as tb
+import pandas as pd
+
+dosya_yolu = r"C:\Users\velid\Downloads\Documents\tablo201122020.pdf"
+df = tb.read_pdf(dosya_yolu,pages="all")
+# tb.convert_into(dosya_yolu,"deneme.csv",output_format="csv")
+
+oku = pd.read_csv(r"C:\Users\velid\Exercism\python\two-fer\deneme.csv",encoding="auto")
+
+df.to_excel()
