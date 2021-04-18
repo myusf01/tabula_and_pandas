7a929cfac71d5d2a8d1da2543665c3db7b4f5799
Zehra <zehraviplus@gmail.com>
2021-04-19 00:06:59 +0300

    yorum eklendi

--- chart.py
+++ chart.py
@@ -2,6 +2,7 @@ from pandas.core.frame import DataFrame
 import tabula as tb
 import pandas as pd
 
+# yapılacak dosya yolunu statik yap.
 dosya_yolu = r"C:\Users\velid\Downloads\Documents\tablo201122020.pdf"
 df = tb.read_pdf(dosya_yolu,pages="all")
 # tb.convert_into(dosya_yolu,"deneme.csv",output_format="csv")
